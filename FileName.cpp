ffd926ff6469c18fea8315b5ec78cbc225945f92
G_9527_W <1968823709@qq.com>
2026-03-20 21:37:24 +0800

    添加了线程池，但是还有许多问题，待完善
    一个简单的线程池

--- FileName.cpp
+++ FileName.cpp
@@ -2,11 +2,14 @@
 #include <memory>
 #include <thread>
 #include <queue>
+#include <vector>
 #include<mutex>
 #include <condition_variable>
 #include <chrono>
+#include <future>
+#include <functional>
+#include <type_traits>
 using namespace std;
-//线程安全队列
 template<typename T>
 class thread_safe_queue
 {
@@ -29,7 +32,7 @@ public:
 	{
 		unique_lock<mutex>lock(mut);
 		cv.wait(lock, [this]() {return !this->q.empty(); });
-		value = q.front();
+		value=move(q.front());
 		q.pop();
 	}
 	bool try_pop(T& value)
@@ -39,7 +42,7 @@ public:
 		{
 			return false;
 		}
-		value = q.front();
+		value=move(q.front());
 		q.pop();
 		return true;
 	}
@@ -50,7 +53,7 @@ public:
 		{
 			return false;
 		}
-		value = q.front();
+		value=move(q.front());
 		q.pop();
 		return true;
 	}
@@ -58,8 +61,6 @@ public:
 	{
 		lock_guard<mutex>lock(mut);
 		return q.empty();
-
-
 	}
 	size_t size() const
 	{
@@ -73,5 +74,120 @@ public:
 		{
 			q.pop();
 		}
+	}
+	void notify_all()
+	{
+		cv.notify_all();
+	}
+};
+
+
+class function_pack
+{
+private:
+
+	class impl_base
+	{
+	public:
+		virtual void call() = 0;
+		virtual ~impl_base() = default;
+	};
+
+	unique_ptr <impl_base>impl;
+	template<typename F>
+	class impl_type :public impl_base
+	{
+	public:
+		F f;
+		impl_type(F&& temp_f) :f(move(temp_f))
+		{
+
+		}
+		void call()
+		{
+			f();
+		}
+	};
+public:
+	template<typename F>
+	function_pack(F&& temp_f) :impl(new impl_type<F>(move(temp_f)))
+	{
+	}
+	function_pack() = default;
+
+	void operator()()
+	{
+		impl->call();
+	}
+	function_pack(function_pack&& fp) noexcept :impl(move(fp.impl))
+	{
+		
+	}
+	function_pack& operator=(function_pack&& fp)noexcept
+	{
+		impl = move(fp.impl);
+		return *this;
+	}
+
+	function_pack(const function_pack& fp) = delete;
+	function_pack& operator=(const function_pack &fp) = delete;
+
+
+};
+
+class thread_pool
+{
+private:
+	thread_safe_queue<function_pack> work_q;
+	vector<thread>threads;
+	atomic <bool> done = false;
+	unsigned thread_num = thread::hardware_concurrency() > 2 ? thread::hardware_concurrency() : 2;
+public:
+	void do_work()
+	{
+		while (!done)
+		{
+			function_pack task;
+			work_q.wait_pop(task);
+			task();
+		}
+	}
+	template<typename func>
+	future<invoke_result_t<func>>submit(func f)
+	{
+		using type = invoke_result_t<func>;
+		packaged_task<type()>task(move(f));
+		future<type>res = task.get_future();
+		work_q.push(move(task));
+		return res;
+		
+	}
+	
+	thread_pool()
+	{
+		
+		for (unsigned i = 0; i < thread_num; i++)
+		{
+			threads.emplace_back(&thread_pool::do_work, this);
+
+		}
+	}
+	~thread_pool()
+	{
+		done = true;
+		for (int i = 0; i < thread_num; i++)
+		{
+			work_q.push(function_pack{});
+		}
+		work_q.notify_all();
+		for (auto &t : threads)
+		{
+			if (t.joinable())
+			{
+				t.join();
+			}
+		}
+		
+		
 	}
 };
